b2178adb2348463cb5ff92bd6a6afbd2204dedf8
TacticalDan <gorksorf@gmail.com>
2021-11-15 15:40:33 -0700

    modToBlockly code looking very well composed!

--- projects/battlefield-portal-script-generator/build-xml.ts
+++ projects/battlefield-portal-script-generator/build-xml.ts
@@ -1,35 +1,27 @@
 type PortalMod = {
-    xml: {
-        mod: {
-            rules: Array<{
-                name: string,
-                eventType: "Ongoing" | "OnGameModeEnding" | "OnGameModeStarted" | "OnMandown" | "OnPlayerDeployed" | "OnPlayerDied" | "OnPlayerEarnedKill" | "OnPlayerIrreversiblyDead" | "OnPlayerJoinGame" | "OnPlayerLeaveGame" | "OnRevived" | "OnTimeLimitReached",
-                objectType: "Global" | "Player" | "Team",
-                conditions: Array<{
-                }>,
-                actions: Array<{
-                }>,
-            }>,
-        },
-    },
+    rules: Array<{
+        name: string,
+        eventType: "Ongoing" | "OnGameModeEnding" | "OnGameModeStarted" | "OnMandown" | "OnPlayerDeployed" | "OnPlayerDied" | "OnPlayerEarnedKill" | "OnPlayerIrreversiblyDead" | "OnPlayerJoinGame" | "OnPlayerLeaveGame" | "OnRevived" | "OnTimeLimitReached",
+        objectType: "Global" | "Player" | "Team",
+        conditions: Array<{
+        }>,
+        actions: Array<{
+        }>,
+    }>,
 };
 
 const exampleMod: PortalMod = {
-    xml: {
-        mod: {
-            rules: [
-                {
-                    name: "Hello World!",
-                    eventType: "Ongoing",
-                    objectType: "Global",
-                    conditions: [
-                    ],
-                    actions: [
-                    ],
-                }
-            ]
+    rules: [
+        {
+            name: "Hello World!",
+            eventType: "Ongoing",
+            objectType: "Global",
+            conditions: [
+            ],
+            actions: [
+            ],
         }
-    },
+    ]
 }
 
 const exampleJSON: any = {
@@ -61,10 +53,10 @@ const exampleJSON: any = {
     }
 };
 
-function jsonToBlocklyXML(key: string, json: any): string {
+function blocklyToXML(key: string, json: any): string {
     if (Array.isArray(json)) {
         return json.reduce((result, next) => {
-            const contents = jsonToBlocklyXML(key, next);
+            const contents = blocklyToXML(key, next);
             return result != ""
                 ? `${result}${contents}`
                 : contents;
@@ -78,22 +70,40 @@ function jsonToBlocklyXML(key: string, json: any): string {
         ? json.value
         : entries
             .filter(([_, value]) => typeof value == "object")
-            .map(([key, value]) => jsonToBlocklyXML(key, value))
+            .map(([key, value]) => blocklyToXML(key, value))
             .join("");
     return `<${key}${properties}>${children}</${key}>`;
 }
 
-console.log(jsonToBlocklyXML("xml", exampleJSON));
-
-function buildXML(mod: PortalMod): string {
-    const { rules } = mod.xml.mod;
-    const generatedRules = rules.map(rule => {
-        return `<block type="ruleBlock">${`<mutation isOnGoingEvent="${rule.eventType == "Ongoing" ? "true" : "false"}"></mutation>`
-            }${`<field name="NAME">${rule.name}</field>`
-            }${`<field name="EVENTTYPE">${rule.eventType}</field>`
-            }${`<field name="OBJECTTYPE">${rule.objectType}</field>`
-            }${`<statement name="CONDITIONS"><block type="conditionBlock"></block></statement>`
-            }</block>`;
-    });
-    return `<xml xmlns="https://developers.google.com/blockly/xml"><block type="modBlock" deletable="false" x="0" y="0"><statement name="RULES"><block type="ruleBlock">${generatedRules}</block></statement></block></xml>`
+function modToBlockly(mod: PortalMod): any {
+    const { rules } = mod;
+    return {
+        xmlns: "https://developers.google.com/blockly/xml",
+        block: {
+            type: "modBlock",
+            deletable: false,
+            x: 0,
+            y: 0,
+            statement: {
+                name: "RULES",
+                block: rules.map(rule => ({
+                    type: "ruleBlock",
+                    mutation: {
+                        isOnGoingEvent: rule.eventType == "Ongoing"
+                    },
+                    field: [
+                        { name: "NAME", value: rule.name },
+                        { name: "EVENTTYPE", value: rule.eventType },
+                        { name: "OBJECTTYPE", value: rule.objectType },
+                    ],
+                    statement: {
+                        name: "CONDITIONS",
+                        block: [],
+                    }
+                })),
+            },
+        },
+    };
 }
+
+console.log(blocklyToXML("xml", modToBlockly(exampleMod)));
