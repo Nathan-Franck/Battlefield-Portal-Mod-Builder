6ba197db8a10907524c93bc906e22403c261e999
TacticalDan <gorksorf@gmail.com>
2021-11-16 01:48:31 -0700

    Approaching a much closer equivalence between hand-built and generated, still a ways to go

--- projects/battlefield-portal-script-generator/build-xml.ts
+++ projects/battlefield-portal-script-generator/build-xml.ts
@@ -1,197 +1,290 @@
 function blocklyToXML(key: string, json: any): string {
-    if (Array.isArray(json)) {
-        return json.reduce((result, next) => {
-            const contents = blocklyToXML(key, next);
-            return result != ""
-                ? `${result}${contents}`
-                : contents;
-        }, "")
-    }
-    const entries = Object.entries(json);
-    const properties = entries
-        .filter(([key, value]) => key != "value" && typeof value != "object")
-        .map(([key, value]) => ` ${key}="${value.toString()}"`).join("");
-    const children = "value" in json
-        ? json.value
-        : entries
-            .filter(([_, value]) => typeof value == "object")
-            .map(([key, value]) => blocklyToXML(key, value))
-            .join("");
-    return `<${key}${properties}>${children}</${key}>`;
+	if (Array.isArray(json)) {
+		return json.reduce((result, next) => {
+			const contents = blocklyToXML(key, next);
+			return result != ""
+				? `${result}${contents}`
+				: contents;
+		}, "")
+	}
+	const entries = Object.entries(json);
+	const properties = entries
+		.filter(([key, value]) => key != "inner" && typeof value != "object")
+		.map(([key, value]) => ` ${key}="${value.toString()}"`).join("");
+	const children = "inner" in json
+		? json.inner
+		: entries
+			.filter(([_, value]) => typeof value == "object")
+			.map(([key, value]) => blocklyToXML(key, value))
+			.join("");
+	return `<${key}${properties}>${children}</${key}>`;
 }
 
 function toLinkedList(key: string, array: any[]) {
-    return array
-        .reverse()
-        .reduce((result, current) => result == null ? current : ({ [key]: current, next: { [key]: result } }), null);
+	return {
+		[key]: array
+			.reverse()
+			.reduce((result, current) => result == null ? current : ({ ...current, next: { [key]: result } }), null)
+	};
 }
 
 type Players =
-    | "EventPlayer"
-    | "EventOtherPlayer"
-    | { readonly ClosestPlayerTo: Vectors }
+	| "EventPlayer"
+	| "EventOtherPlayer"
+	| { readonly ClosestPlayerTo: Vectors }
 
 type Numbers =
-    | { readonly GetGameModeScore: Players }
-    | "GetGameModeTargetScore"
-    | { readonly DistanceBetween: readonly [Vectors, Vectors] }
-    | number
+	| "GetGameModeTargetScore"
+	| { readonly GetGameModeScore: Players }
+	| { readonly DistanceBetween: readonly [Vectors, Vectors] }
+	| number
 
 type Booleans =
-    | { readonly NotEqualTo: readonly [Players, Players] }
-    | { readonly NotEqualTo: readonly [Numbers, Numbers] }
-    | { readonly Equals: readonly [Players, Players] }
-    | { readonly Equals: readonly [Numbers, Numbers] }
-    | { readonly LessThan: readonly [Numbers, Numbers] }
-    | { readonly GetPlayerState: readonly [Players, PlayerStateBool] }
+	| { readonly NotEqualTo: readonly [Players, Players] }
+	| { readonly NotEqualTo: readonly [Numbers, Numbers] }
+	| { readonly Equals: readonly [Players, Players] }
+	| { readonly Equals: readonly [Numbers, Numbers] }
+	| { readonly LessThan: readonly [Numbers, Numbers] }
+	| { readonly GetPlayerState: readonly [Players, SoldierStateBool] }
+	| boolean
 
 type Voids =
-    | { readonly SetGameModeScore: readonly [Players, Numbers] }
-    | { readonly EndGameMode: Players }
+	| { readonly SetGameModeScore: readonly [Players, Numbers] }
+	| { readonly EndGameMode: Players }
+	| { readonly EnableDefaultScoring: Booleans }
+	| { readonly SetRoundTimeLimit: Numbers }
+	| { readonly SetTargetScore: Numbers }
 
 type Vectors =
-    { readonly GetPlayerState: readonly [Players, PlayerStateVector] }
+	{ readonly GetPlayerState: readonly [Players, SoldierStateVector] }
 
-type PlayerStateVector =
-    | "Position"
-    | "Linear Velocity";
+type PortalValues = Players | Numbers | Booleans | Voids | SoldierStateVector | SoldierStateBool | SoldierStateNumber;
 
-type PlayerStateBool =
-    | "Is AI Soldier"
-    | "Is Alive"
-    | "Is Being Revived"
-    | "Is Crouching"
-    | "Is Dead"
-    | "Is Firing"
-    | "Is In Air"
-    | "Is Interacting"
-    | "Is In Vehicle"
-    | "Is In Water"
-    | "Is Jumping"
-    | "Is Mandown"
-    | "Is On Ground"
-    | "Is Parachuting"
-    | "Is Prone"
-    | "Is Reloading"
-    | "Is Reviving"
-    | "Is Sprinting"
-    | "Is Standing"
-    | "Is Vaulting"
-    | "Is Zooming";
+const playerStateAccessors = <const>[
+	{
+		type: "SoldierStateVector",
+		name: [
+			"GetPosition",
+			"GetLinearVelocity"
+		],
+	},
+	{
+		type: "SoldierStateBool",
+		name: [
+			"IsAISoldier",
+			"IsAlive",
+			"IsBeingRevived",
+			"IsCrouching",
+			"IsDead",
+			"IsFiring",
+			"IsInAir",
+			"IsInteracting",
+			"IsInVehicle",
+			"IsInWater",
+			"IsJumping",
+			"IsMandown",
+			"IsOnGround",
+			"IsParachuting",
+			"IsProne",
+			"IsReloading",
+			"IsReviving",
+			"IsSprinting",
+			"IsStanding",
+			"IsVaulting",
+			"IsZooming",
+		],
+	},
+	{
+		type: "SoldierStateNumber",
+		name: [
+			"CurrentHealth",
+			"CurrentInventoryAmmo",
+			"CurrentInventoryMagazineAmmo",
+			"MaxHealth",
+			"NormalizedHealth",
+			"Speed",
+		],
+	}
+];
 
-type PlayerStateNumber =
-    | "Current Health"
-    | "Current Inventory Ammo"
-    | "Current Inventory Magazine Ammo"
-    | "Max Health"
-    | "Normalized Health"
-    | "Speed"
+type SoldierStateVector = (typeof playerStateAccessors[number] & { type: "SoldierStateVector" })["name"][number];
+type SoldierStateBool = (typeof playerStateAccessors[number] & { type: "SoldierStateBool" })["name"][number]
+type SoldierStateNumber = (typeof playerStateAccessors[number] & { type: "SoldierStateNumber" })["name"][number]
 
 
 type PortalMod = {
-    rules: Array<
-        & {
-            name: string,
-            conditions: Array<Booleans>,
-            actions: Array<Voids>,
-        }
-        & (
-            | {
-                eventType:
-                | "OnGameModeEnding"
-                | "OnGameModeStarted"
-                | "OnMandown"
-                | "OnPlayerDeployed"
-                | "OnPlayerDied"
-                | "OnPlayerEarnedKill"
-                | "OnPlayerIrreversiblyDead"
-                | "OnPlayerJoinGame"
-                | "OnPlayerLeaveGame"
-                | "OnRevived"
-                | "OnTimeLimitReached",
-            }
-            | {
-                eventType: "Ongoing",
-                objectType: "Global" | "Player" | "Team",
-            }
-        )
-    >,
+	rules: Array<
+		& {
+			name: string,
+			conditions: Array<Booleans>,
+			actions: Array<Voids>,
+		}
+		& (
+			| {
+				eventType:
+				| "OnGameModeEnding"
+				| "OnGameModeStarted"
+				| "OnMandown"
+				| "OnPlayerDeployed"
+				| "OnPlayerDied"
+				| "OnPlayerEarnedKill"
+				| "OnPlayerIrreversiblyDead"
+				| "OnPlayerJoinGame"
+				| "OnPlayerLeaveGame"
+				| "OnRevived"
+				| "OnTimeLimitReached",
+			}
+			| {
+				eventType: "Ongoing",
+				objectType: "Global" | "Player" | "Team",
+			}
+		)
+	>,
 };
 
-function modToBlockly(mod: PortalMod): any {
-    const { rules } = mod;
-    return {
-        xmlns: "https://developers.google.com/blockly/xml",
-        block: {
-            type: "modBlock",
-            deletable: false,
-            x: 0,
-            y: 0,
-            statement: {
-                name: "RULES",
-                ...toLinkedList("block", rules.map(rule => ({
-                    type: "ruleBlock",
-                    mutation: {
-                        isOnGoingEvent: rule.eventType == "Ongoing"
-                    },
-                    field: [
-                        { name: "NAME", value: rule.name },
-                        { name: "EVENTTYPE", value: rule.eventType },
-                        ...(rule.eventType != "Ongoing" ? [] : [{ name: "OBJECTTYPE", value: rule.objectType }]),
-                    ],
-                    statement: {
-                        name: "CONDITIONS",
-                        block: [],
-                    }
-                }))),
-            },
-        },
-    };
+function parseValue(value: PortalValues): any {
+	if (typeof value == "string") {
+		const filteredAccessors = playerStateAccessors.filter(accessors => accessors.name.findIndex(name => name == value) >= 0)
+		if (filteredAccessors.length > 0) {
+			const accessors = filteredAccessors[0];
+			return {
+				type: `${accessors.name}Item`,
+				field: [
+					{ name: "VALUE-0", inner: accessors.name },
+					{ name: "VALUE-1", inner: value },
+				],
+			};
+		}
+		return {
+			type: value,
+		};
+	}
+	if (typeof value == "number") {
+		return {
+			type: "Number",
+			field: {
+				name: "NUM",
+				inner: 900,
+			},
+		};
+	}
+	if (typeof value == "boolean") {
+		return {
+			type: "Boolean",
+			field: {
+				name: "BOOL",
+				inner: `${value}`.toUpperCase(),
+			},
+		};
+	}
+	if (typeof value == "object") {
+		const [type, parameters] = Object.entries(value)[0];
+		return {
+			type,
+			...(parameters != "object"
+				? {
+					value: {
+						name: "VALUE-0",
+						block: parseValue(parameters),
+					},
+				}
+				: toLinkedList("value", parameters.map((parameter: any, index: number) => ({
+					name: `VALUE-${index}`,
+					block: parseValue(parameter),
+				})))),
+		};
+	}
+	return { "error": true };
 }
 
-function PlayerPosition(player: Players): Vectors { return { GetPlayerState: [player, "Position"] } };
+function modToBlockly(mod: PortalMod): any {
+	const { rules } = mod;
+	return {
+		xmlns: "https://developers.google.com/blockly/xml",
+		block: {
+			type: "modBlock",
+			deletable: false,
+			x: 0,
+			y: 0,
+			statement: {
+				name: "RULES",
+				...toLinkedList("block", rules.map(rule => ({
+					type: "ruleBlock",
+					mutation: {
+						isOnGoingEvent: rule.eventType == "Ongoing"
+					},
+					field: [
+						{ name: "NAME", inner: rule.name },
+						{ name: "EVENTTYPE", inner: rule.eventType },
+						...(rule.eventType != "Ongoing" ? [] : [{ name: "OBJECTTYPE", inner: rule.objectType }]),
+					],
+					statement: [
+						{
+							name: "CONDITIONS",
+							...toLinkedList("block", rule.conditions.map(condition => ({
+								block: "conditionBlock",
+								...parseValue(condition),
+							}))),
+						},
+						{
+							name: "ACTIONS",
+							...toLinkedList("block", rule.actions.map(parseValue)),
+						}
+					]
+				}))),
+			},
+		},
+	};
+}
+
+function PlayerPosition(player: Players): Vectors { return { GetPlayerState: [player, "GetPosition"] } };
 
 function ClosestPlayerDistance(player: Players): Numbers {
-    return {
-        DistanceBetween: [
-            { GetPlayerState: [{ ClosestPlayerTo: PlayerPosition(player) }, "Position"] },
-            PlayerPosition(player)
-        ]
-    };
+	return {
+		DistanceBetween: [
+			{ GetPlayerState: [{ ClosestPlayerTo: PlayerPosition(player) }, "GetPosition"] },
+			PlayerPosition(player)
+		]
+	};
 };
 
 const exampleMod: PortalMod = {
-    rules: [
-        {
-            name: "Hello World!",
-            eventType: "Ongoing",
-            objectType: "Global",
-            conditions: [
-                { NotEqualTo: ["EventPlayer", "EventOtherPlayer"] },
-            ],
-            actions: [
-            ],
-        },
-        {
-            name: "Increment Score",
-            eventType: "OnPlayerEarnedKill",
-            conditions: [
-                { Equals: [{ GetGameModeScore: "EventPlayer" }, "GetGameModeTargetScore"] },
-            ],
-            actions: [
-                { EndGameMode: "EventPlayer" }
-            ],
-        },
-        {
-            name: "Proximity Tag",
-            eventType: "OnPlayerEarnedKill",
-            conditions: [
-                { LessThan: [ClosestPlayerDistance("EventPlayer"), 1] }
-            ],
-            actions: [
-                { EndGameMode: "EventPlayer" }
-            ],
-        }
-    ],
+	rules: [
+		{
+			name: "Hello World!",
+			eventType: "Ongoing",
+			objectType: "Global",
+			conditions: [
+				{ NotEqualTo: ["EventPlayer", "EventOtherPlayer"] },
+			],
+			actions: [
+				{ EnableDefaultScoring: false },
+				{ SetRoundTimeLimit: 900 },
+				{ SetTargetScore: 50 },
+			],
+		},
+		{
+			name: "Increment Score",
+			eventType: "OnPlayerEarnedKill",
+			conditions: [
+				{ Equals: [{ GetGameModeScore: "EventPlayer" }, "GetGameModeTargetScore"] },
+			],
+			actions: [
+				{ EndGameMode: "EventPlayer" }
+			],
+		},
+		{
+			name: "Proximity Tag",
+			eventType: "OnPlayerEarnedKill",
+			conditions: [
+				{ LessThan: [ClosestPlayerDistance("EventPlayer"), 1] }
+			],
+			actions: [
+				{ EndGameMode: "EventPlayer" }
+			],
+		}
+	],
 };
+console.log(JSON.stringify(modToBlockly(exampleMod), null, 4));
+console.log("");
 console.log(blocklyToXML("xml", modToBlockly(exampleMod)));
